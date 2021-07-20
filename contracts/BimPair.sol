1c6db0857c1cd96c62a58f13ead64638a8ca7ced
muraz <nukul.muenjihan@gmail.com>
2021-07-20 16:27:03 +0700

    rename bim

--- contracts/BimPair.sol
+++ contracts/BimPair.sol
@@ -169,7 +169,7 @@ contract BimPair is IBimPair, BimERC20 {
         require(to != _token0 && to != _token1, 'Bim: INVALID_TO');
         if (amount0Out > 0) _safeTransfer(_token0, to, amount0Out); // optimistically transfer tokens
         if (amount1Out > 0) _safeTransfer(_token1, to, amount1Out); // optimistically transfer tokens
-        if (data.length > 0) IBimCallee(to).pancakeCall(msg.sender, amount0Out, amount1Out, data);
+        if (data.length > 0) IBimCallee(to).bimCall(msg.sender, amount0Out, amount1Out, data);
         balance0 = IERC20(_token0).balanceOf(address(this));
         balance1 = IERC20(_token1).balanceOf(address(this));
         }
--- contracts/interfaces/IBimCallee.sol
+++ contracts/interfaces/IBimCallee.sol
@@ -1,5 +1,5 @@
 pragma solidity >=0.5.0;
 
 interface IBimCallee {
-    function pancakeCall(address sender, uint amount0, uint amount1, bytes calldata data) external;
+    function bimCall(address sender, uint amount0, uint amount1, bytes calldata data) external;
 }
